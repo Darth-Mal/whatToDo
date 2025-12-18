3a67769482c492f8ef3b641c911d425d350d7809
Victor Malomo <malomovictor@gmail.com>
2025-12-18 23:05:24 +0100

    adjusted logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [todoItem, setTodoItem] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [numbering, setNumbering] = useState(1);
+
+  const addTodo = () => {
+    if (inputValue.trim() === "") return;
+    // Logic to add todo item
+
+    const newItem = {
+      id: numbering,
+      text: inputValue,
+      status: undone,
+    };
+
+    setNumbering(numbering++);
+    setTodoItem([...todoItem, newItem]);
+    setInputValue("");
+
+    console.log(todoItem);
+  };
+
   return (
     <>
-      <div>hhhhhhhhhhhhhhh</div>
+      <h1>Todo List</h1>
+
+      <div className="listOfItems">
+        <ul>
+          {todoItem.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="todoInput">
+        <input
+          type="text"
+          name="todo"
+          id=""
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
     </>
   );
 }
